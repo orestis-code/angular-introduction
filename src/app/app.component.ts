6ed0bf5b5da76b766f5bb12f085f0c06e4aa4c1f
Or.Apatzidis <orestisphys@gmail.com>
2024-04-01 12:17:38 +0300

    component input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PersonTableComponent } from './components/person-table/person-table.component';
+import { Person } from './shared/interfaces/person';
 
 
 @Component({
@@ -12,5 +13,19 @@ import { PersonTableComponent } from './components/person-table/person-table.com
 export class AppComponent {
   name: string = 'Orestis';
 
-  
+  person0: Person = {
+    givenName: 'Orestis',
+    surName: 'Apatzidis',
+    age: 0x20,
+    email: 'orestisphys@gmail.com',
+    address: 'Athens, Greece'
+  };
+
+  person1: Person = {
+    givenName: 'John',
+    surName: 'Doe',
+    age: 0x25,
+    email: 'jdoe@example.com',
+    address: 'New York, USA'
+  };
 }
