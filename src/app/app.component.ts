f4bd166cd976e53b600dd46aea547a8d4470b316
Or.Apatzidis <orestisphys@gmail.com>
2024-04-03 10:31:07 +0300

    @for directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { PersonTableComponent } from './components/person-table/person-table.component';
 import { Person } from './shared/interfaces/person';
 
-
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -13,7 +12,7 @@ import { Person } from './shared/interfaces/person';
 export class AppComponent {
   name: string = 'Orestis';
 
-  person0: Person = {
+  person0 = {
     givenName: 'Orestis',
     surName: 'Apatzidis',
     age: 0x20,
@@ -21,11 +20,85 @@ export class AppComponent {
     address: 'Athens, Greece'
   };
 
-  person1: Person = {
-    givenName: 'John',
-    surName: 'Doe',
+  person1 = {
+    givenName: 'Simos',
+    surName: 'Panagiotoglou',
     age: 0x25,
-    email: 'jdoe@example.com',
+    email: 'sp@example.com',
     address: 'New York, USA'
   };
+
+  users: Person[] = 
+  [
+    {
+      givenName: 'John',
+      surName: 'Doe',
+      age: 30,
+      email: 'john.doe@example.com',
+      address: '123 Main St',
+    },
+    {
+      givenName: 'Jane',
+      surName: 'Doe',
+      age: 28,
+      email: 'jane.doe@example.com',
+      address: '123 Main St',
+    },
+    {
+      givenName: 'Jim',
+      surName: 'Brown',
+      age: 45,
+      email: 'jim.brown@example.com',
+      address: '456 Park Ave',
+    },
+    {
+      givenName: 'Jill',
+      surName: 'Brown',
+      age: 42,
+      email: 'jill.brown@example.com',
+      address: '456 Park Ave',
+    },
+    {
+      givenName: 'Jake',
+      surName: 'Smith',
+      age: 36,
+      email: 'jake.smith@example.com',
+      address: '789 Broadway',
+    },
+    {
+      givenName: 'Judy',
+      surName: 'Smith',
+      age: 34,
+      email: 'judy.smith@example.com',
+      address: '789 Broadway',
+    },
+    {
+      givenName: 'Jack',
+      surName: 'Johnson',
+      age: 50,
+      email: 'jack.johnson@example.com',
+      address: '321 Oak St',
+    },
+    {
+      givenName: 'Julie',
+      surName: 'Johnson',
+      age: 48,
+      email: 'julie.johnson@example.com',
+      address: '321 Oak St',
+    },
+    {
+      givenName: 'Jerry',
+      surName: 'Davis',
+      age: 55,
+      email: 'jerry.davis@example.com',
+      address: '654 Pine St',
+    },
+    {
+      givenName: 'June',
+      surName: 'Davis',
+      age: 53,
+      email: 'june.davis@example.com',
+      address: '654 Pine St',
+    },
+  ];
 }
