036d0bac6d31e1c49f78c203f51e555c368b084f
Or.Apatzidis <orestisphys@gmail.com>
2024-04-08 10:07:29 +0300

    angular material

--- src/app/components/component-output-example/component-output-example.component.ts
+++ src/app/components/component-output-example/component-output-example.component.ts
@@ -1,32 +1,32 @@
 import { Component, Inject } from '@angular/core';
 import { EPerson, ManyPerson } from 'src/app/shared/interfaces/person';
 import { SimpleDatatableComponent } from '../simple-datatable/simple-datatable.component';
-// import {
-//   Dialog,
-//   DialogRef,
-//   DIALOG_DATA,
-//   DialogModule,
-// } from '@angular/cdk/dialog';
-// import { PersonTableComponent } from '../person-table/person-table.component';
+import {
+  Dialog,
+  DialogRef,
+  DIALOG_DATA,
+  DialogModule,
+} from '@angular/cdk/dialog';
+import { PersonTableComponent } from '../person-table/person-table.component';
 
 @Component({
   selector: 'app-component-output-example',
   standalone: true,
-  imports: [SimpleDatatableComponent],
+  imports: [DialogModule, SimpleDatatableComponent],
   templateUrl: './component-output-example.component.html',
   styleUrl: './component-output-example.component.css',
 })
 export class ComponentOutputExampleComponent {
   manyPerson = ManyPerson;
 
-  // constructor(public dialog: Dialog) {}
+  constructor(public dialog: Dialog) {}
 
   onPersonClicked(person: EPerson) {
     // console.log(person);
-    alert(this.personTemplate(person));
-    // this.dialog.open(PersonDialogComponent, {
-    //   data: person,
-    // });
+    // alert(this.personTemplate(person));
+    this.dialog.open(PersonDialogComponent, {
+      data: person,
+    });
   }
 
   personTemplate(person: EPerson) {
@@ -42,30 +42,30 @@ export class ComponentOutputExampleComponent {
   }
 }
 
-// @Component({
-//   imports: [PersonTableComponent],
-//   standalone: true,
-//   template: `
-//     <app-person-table [person]="person"></app-person-table>
-//     <button class="btn btn-primary btn-sm" (click)="dialogRef.close()">
-//       Close
-//     </button>
-//   `,
-//   styles: [
-//     `
-//       :host {
-//         display: block;
-//         background: #fff;
-//         border-radius: 8px;
-//         padding: 16px;
-//         max-width: 500px;
-//       }
-//     `,
-//   ],
-// })
-// class PersonDialogComponent {
-//   constructor(
-//     public dialogRef: DialogRef,
-//     @Inject(DIALOG_DATA) public person: EPerson,
-//   ) {}
-// }
+@Component({
+  imports: [PersonTableComponent],
+  standalone: true,
+  template: `
+    <app-person-table [person]="person"></app-person-table>
+    <button class="btn btn-primary btn-sm" (click)="dialogRef.close()">
+      Close
+    </button>
+  `,
+  styles: [
+    `
+      :host {
+        display: block;
+        background: #fff;
+        border-radius: 8px;
+        padding: 16px;
+        max-width: 500px;
+      }
+    `,
+  ],
+})
+class PersonDialogComponent {
+  constructor(
+    public dialogRef: DialogRef,
+    @Inject(DIALOG_DATA) public person: EPerson,
+  ) {}
+}
