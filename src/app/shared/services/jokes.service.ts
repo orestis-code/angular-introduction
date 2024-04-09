529cb91e12baf71bdf113f62c6cca0f30203e2e9
Or.Apatzidis <orestisphys@gmail.com>
2024-04-09 19:24:08 +0300

    ΗTTP Client Service

--- src/app/shared/services/jokes.service.ts
+++ src/app/shared/services/jokes.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
+import { ChuckNorrisJoke, DadJoke } from '../interfaces/jokes';
 
 const DAD_JOKES_API_URL = 'https://icanhazdadjoke.com/';
-const JACK_NORRIS_JOKES_API_URL = 'https://api.chucknorris.io/jokes/random';
+const CHUCK_NORRIS_JOKES_API_URL = 'https://api.chucknorris.io/jokes/random';
 
 @Injectable({
   providedIn: 'root'
@@ -10,4 +11,20 @@ const JACK_NORRIS_JOKES_API_URL = 'https://api.chucknorris.io/jokes/random';
 export class JokesService {
 
   http: HttpClient = inject(HttpClient);
+
+  getDadJoke() {
+    return this.http.get<DadJoke>(DAD_JOKES_API_URL, {
+      headers: {
+        Accept: 'application/json',
+      },
+    });
+  }
+  
+  getChuckNorrisJoke() {
+    return this.http.get<ChuckNorrisJoke>(CHUCK_NORRIS_JOKES_API_URL, {
+      headers: {
+        Accept: 'application/json',
+      },
+    });
+  }
 }
